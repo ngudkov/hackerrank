66bac2d894b7dec39d17c1ce9a969f03ccec5a6b
ngudkov <nicolkka@gmail.com>
2019-09-17 18:22:55 +0300

    Update merge_the_tools.py

--- merge_the_tools.py
+++ merge_the_tools.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python36
 
 from collections import OrderedDict
+
 def merge_the_tools(string, k):
     for i in range(0,len(string),k):
         print(''.join(OrderedDict.fromkeys(string[i:i+k])))
-    # your code goes here
 
 if __name__ == '__main__':
     string, k = input(), int(input())
